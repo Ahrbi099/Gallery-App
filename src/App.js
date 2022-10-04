0b2453a313d287135bd28961dc916ed830186a83
Ahrbi099 <113051728+Ahrbi099@users.noreply.github.com>
2022-10-04 09:49:01 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Searchbar from './Components/Searchbar';
+import Imagesection from './Components/Imagesection';
+
+
+function App() {
+
+  
+ return (
+  <div>
+  <Searchbar />
+  <Imagesection/>
+
+  </div>
+ )
+}
+
+export default App;
